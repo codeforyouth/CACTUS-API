eed871c573a211c10369315f2e2cdbfbfa5c304f
nakao107107 <imakeanewstory@gmail.com>
2019-09-05 11:37:37 +0900

    モデルにリレーション追加

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -7,4 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     protected $fillable = ['description', 'link', 'github_url', 'user_id', 'status'];
+
+    public function tags()
+    {
+        return $this->hasMany('App\Models\Tag');
+    }
+
+    public function codings()
+    {
+        return $this->hasMany('App\Models\Coding');
+    }
+
+    public function problems()
+    {
+        return $this->hasMany('App\Models\Problem');
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany('App\Models\Review');
+    }
 }
