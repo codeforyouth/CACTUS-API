70adc8fa8cff57eb453ab8ff2008dd08cab2b428
nakao107107 <imakeanewstory@gmail.com>
2019-10-19 17:12:43 +0900

    solution model作成

---
+++ database/migrations/2019_10_19_074418_create_solutions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSolutionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('solutions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('repository_url');
+            $table->text('link');
+            $table->text('note');
+            $table->integer('issue_id');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('solutions');
+    }
+}
